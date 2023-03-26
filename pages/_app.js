a38124d7df3d1a3016dab336fdeeef36dd49d888
Ahmed Musawir <moose@Ahmeds-Mac.local>
2023-03-26 11:43:03 +0800

    26Mar2023 - first commit - ready to start project

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.scss';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
